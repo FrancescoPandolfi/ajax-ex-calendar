efe2611ed525fab33a02bc9a37cb07dd72d6fbb1
Francesco Pandolfi <francesco.pan@gmail.com>
2020-02-03 16:07:59 +0100

    Inseriti giorni nella lista con attributo

--- script.js
+++ script.js
@@ -16,39 +16,9 @@ $(document).ready(function () {
   success: function (data, stato) {
     var response = data.response;
     console.log(response);
-    console.log(response[0].name);
-    console.log(response[0].date);
-    console.log(moment(response[0].date).locale('it').format(`DDD dddd`));
-
-
-    // Gennaio
-
-    // Imposta la data al 1 gennaio 2018
-    var get2018 = moment().locale('it').set({'year' : 2018, 'month' : 0, 'date' : 1});
-
-    // prende il numero dei giorni presenti in questo mese
-    var daysIn = get2018.daysInMonth(0);
-
-    // Prende il primo giorno dell'anno
-    var giorno1 = get2018.format(`DDD dddd`);
-    var gennaio = [giorno1];
-
-    // ciclo che genera un array di giorni
-    for (var i = 1; i < daysIn; i++) {
-        gennaio.push(get2018.add(1, 'day').format(`DDD dddd`));
-    }
-    console.log(gennaio);
-
-
-    gennaio.forEach(function(item) {
-      var context = { 'giorno' : item};
-      var html = template(context);
-      $('.gennaio').append(html)
-    });
-
-
-
-
+    // console.log(response[0].name);
+    // console.log(response[0].date);
+    console.log(moment(response[1].date).locale('it').format(`DDD dddd`));
 
     // response.forEach(function (obj, i) {
     //   console.log(obj);
@@ -66,6 +36,33 @@ $(document).ready(function () {
   }
 });
 
+
+
+// Gennaio
+
+// Imposta la data al 1 gennaio 2018
+var get2018 = moment().locale('it').set({'year' : 2018, 'month' : 0, 'date' : 1});
+
+// prende il numero dei giorni presenti in questo mese
+var daysIn = get2018.daysInMonth(0);
+
+// Prende il primo giorno dell'anno e lo trasforma nel formato da stampare nell'html
+var giorno = get2018.format(`DDD dddd`);
+
+// Prende il primo giorno dell'anno e lo trasforma nel formato da mettere nel data-day
+var dataDay = get2018.format(`YYYY-MM-DD`);
+
+// ciclo che genera un array di giorni
+for (var i = 0; i < daysIn; i++) {
+
+    var context = {'giorno' : giorno, 'day' : dataDay};
+    var html = template(context);
+    $('.gennaio').append(html)
+    giorno = get2018.add(1, 'days').format(`DDD dddd`);
+    dataDay = get2018.format(`YYYY-MM-DD`)
+
+
+}
 
 
 
